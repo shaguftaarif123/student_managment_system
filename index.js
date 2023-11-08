b2199a6135f528e10e0f825a09cd187790dcc2b6
shaguftaarif123 <143534829+shaguftaarif123@users.noreply.github.com>
2023-11-08 11:23:53 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+class School {
+    addStudent(stdObj) {
+        this.students.push(stdObj);
+    }
+    addTeacher(teachObj) {
+        this.teachers.push(teachObj);
+    }
+    constructor(name) {
+        this.students = [];
+        this.teachers = [];
+        this.name = name;
+    }
+}
+class Student {
+    constructor(name, age, schoolName) {
+        this.name = name;
+        this.age = age;
+        this.schoolName = schoolName;
+    }
+}
+class Teacher extends Student {
+}
+let school1 = new School("Alpha");
+let school2 = new School("City");
+let s1 = new Student("Shag", 40, school1.name);
+let s2 = new Student("Bilo", 45, school1.name);
+let s3 = new Student("Arif", 50, school1.name);
+let t1 = new Teacher("Hadi", 60, school1.name);
+let t2 = new Teacher("Zia", 70, school2.name);
+let t3 = new Teacher("Imran", 75, school2.name);
+school1.addStudent(s1);
+school2.addStudent(s2);
+school2.addStudent(s3);
+school1.addTeacher(t1);
+school2.addTeacher(t2);
+school2.addTeacher(t3);
+// console.log(t1)
+// console.log(t2)
+// console.log(t3)
+console.log(school1);
+console.log(school2);
+export {};
